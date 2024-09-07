f493f9dd0ce04371b3938f63cfcb31d12fbc99de
lucas <paradisi.lucas@gmail.com>
2024-09-07 15:24:49 -0300

    Adding Express service

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Happy developing ✨')
+const express = require('express')
+const app = express()
+
+require('dotenv').config();
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
+module.exports = app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
